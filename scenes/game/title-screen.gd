5775dce8f532fd150bb82eb958639210bf1a4598
Marc Meinhardt <49322832+MarcMeinhardt@users.noreply.github.com>
2020-04-24 07:51:49 +0200

    Add files via upload

---
+++ scenes/game/title-screen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var scene_path_to_load
+
+func _ready():
+   $UIMarginContainer/VBoxContainer/UiLowMidMarginContainer3/MenuButtonsContainer/CenterRow/Buttons/SinglePlayerButton.grab_focus()
+   for button in $UIMarginContainer/VBoxContainer/UiLowMidMarginContainer3/MenuButtonsContainer/CenterRow/Buttons.get_children():
+      button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+      
+func _on_Button_pressed(scene_to_load):
+   scene_path_to_load = scene_to_load
+   $FadeIn.show()
+   $FadeIn.fade_in()
+   $UIMarginContainer.hide()
+
+func _on_FadeIn_fade_finished():
+   get_tree().change_scene(scene_path_to_load)
